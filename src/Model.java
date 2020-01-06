dc54e0e2de4fb1e570ef794fa69e1e760ea2cbfe
Nolan Lin Zosso <nolanlin.zosso@edu.hefr.ch>
2020-01-06 11:36:07 +0100

    Created the recordData method in Model.

--- src/Model.java
+++ src/Model.java
@@ -1,14 +1,52 @@
+import java.io.*;
+
 public class Model {
     private Controler ctrl;
+    private File scoreFile;
+    private BufferedReader fileReader;
+    private BufferedWriter fileWriter;
+
+    public Model(){
+        scoreFile = new File((System.getProperty("user.home")
+                + "/Desktop/scores.txt").replace("\\", "/"));
+    }
+
     /**
-     *
      * @param name
      * @param score
      */
     public void recordData(String name, int score) {
-        //database query here
+        String tmpScores = "";
+        if(scoreFile.exists()){
+            try {
+                fileReader = new BufferedReader(new FileReader(scoreFile));
+                String tmp = fileReader.readLine();
+                while(tmp != null){
+                    tmpScores += tmp + "\n";
+                    tmp = fileReader.readLine();
+                }
+                tmpScores += name + " : " + score;
+                fileReader.close();
+            } catch (IOException e){
+                System.err.println("An IOEXception happened.");
+                e.printStackTrace();
+            }
+        }else{
+            tmpScores = name + " : " + score;
+        }
+
+        try {
+            fileWriter = new BufferedWriter(new OutputStreamWriter(
+                    new FileOutputStream(scoreFile), "utf-8"));
+            fileWriter.write(tmpScores);
+            fileWriter.close();
+        } catch (IOException e) {
+            System.err.println("An IOEXception happened.");
+            e.printStackTrace();
+        }
     }
-    public void setCtrl(Controler ctrl){
+
+    public void setCtrl(Controler ctrl) {
         this.ctrl = ctrl;
     }
 
